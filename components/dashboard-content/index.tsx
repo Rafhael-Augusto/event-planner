df7b5d9e236d107bc1d911ef85b6f87f68d64e7d
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-18 17:53:09 -0300

    feat: add dashboard-content component

--- components/dashboard-content/index.tsx
+++ components/dashboard-content/index.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export async function DashboardContent(userId: { userId: string | undefined }) {
   return (
-    <div>
-      <div>
+    <div className="flex flex-1 flex-col gap-6">
+      <div className="flex flex-wrap items-center justify-around gap-3">
         <div>
-          <h1>Seus Eventos</h1>
-          <p>Track attendee responses and manage invite links.</p>
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Seus Eventos
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Track attendee responses and manage invite links.
+          </p>
         </div>
+
+        <Button asChild>
+          <Link href={"/event/new"}>Criar evento</Link>
+        </Button>
       </div>
     </div>
   );
