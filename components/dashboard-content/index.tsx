7c092020470c18eb94d58151cc842c3ecbe3081b
Rafhael <rafhaelaugustodev@gmail.com>
2026-04-18 17:46:24 -0300

    feat: initial commit

---
+++ components/dashboard-content/index.tsx
@@ -0,0 +1,12 @@
+export async function DashboardContent(userId: { userId: string | undefined }) {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Seus Eventos</h1>
+          <p>Track attendee responses and manage invite links.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
